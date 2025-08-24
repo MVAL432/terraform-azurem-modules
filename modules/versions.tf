c9de8081371147e5a0ac3fe6763841a4f0cb01b8
Anand Lokesh <anandlokesh154@gmail.com>
2025-08-24 13:08:48 -0400

    Create versions.tf

---
+++ modules/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "73c7706f-2d84-449b-b2b5-7d62c30654ff"
+}
