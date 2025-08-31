438570f5ff36c449519fae63832b6cbc853ccddc
Anand Lokesh <anandlokesh154@gmail.com>
2025-08-31 11:31:27 -0400

    Update outputs.tf

--- modules/nsg/outputs.tf
+++ modules/nsg/outputs.tf
@@ -8,3 +8,9 @@ output "network_security_group_name" {
   description = "The name of the Network Security Group"
   value       = azurerm_network_security_group.nsg.name
 }
+
+output "network_security_group_id" {
+  description = "The ID of the Network Security Group"
+  value       = azurerm_network_security_group.nsg.id
+  
+}
